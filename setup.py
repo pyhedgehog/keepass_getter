e6bdb3a7b2abb9abaeb767b6ee9e8119a21e01ed
PyHedgehog <pyhedgehog@users.noreply.github.com>
2021-02-18 14:57:12 +0300

    Fix project url in setup.py
    This URL is used on pypi.org page of this project.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='keepass_getter',
       version='1.0.0',
       description='Python library to retrieve passwords from CLI & Python using KeepassHTTP',
-      url='https://github.com/M-Gregoire/keepass-getter',
+      url='https://github.com/M-Gregoire/keepass_getter',
       author='Grégoire Martinache',
       license='Apache-2.0',
       packages=['keepass_getter'],
